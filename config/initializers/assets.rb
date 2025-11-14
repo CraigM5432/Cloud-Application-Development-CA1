4ad970048f1a2b119968b48f1b3e98097d63a42a
CraigM5432 <murphy.craig93@gmail.com>
2025-11-14 11:33:53 +0000

    in class deployment

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,3 +8,5 @@ Rails.application.config.assets.version = "1.0"
 Rails.application.config.assets.paths << Rails.root.join("node_modules/bootstrap-icons/font")
 Rails.application.config.assets.paths << Rails.root.join("node_modules/bootstrap/dist/js")
 Rails.application.config.assets.precompile << "bootstrap.bundle.min.js"
+# Use Bun instead of Yarn for CSS bundling
+Rails.application.config.cssbundling.command = "bun build:css"
